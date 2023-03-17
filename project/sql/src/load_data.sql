1465ecd10d7fe85435430bbc598de82e4b40d689
foocypher <67489604+aleclb223@users.noreply.github.com>
2023-03-16 20:16:07 -0700

    changed data path

--- project/sql/src/load_data.sql
+++ project/sql/src/load_data.sql
@@ -1,36 +1,37 @@
+
 COPY Users
-FROM 'users.csv' 
+FROM '/extra/abogh005/project/data/users.csv' 
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE users_userID_seq RESTART 101; 
 
 COPY Hotel
-FROM 'hotels.csv'
+FROM '/extra/abogh005/project/data/hotels.csv'
 WITH DELIMITER ',' CSV HEADER;   
 
 COPY Rooms
-FROM 'rooms.csv'
+FROM '/extra/abogh005/project/data/rooms.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY MaintenanceCompany
-FROM 'company.csv'
+FROM '/extra/abogh005/project/data/company.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY RoomBookings
-FROM 'bookings.csv'
+FROM '/extra/abogh005/project/data/bookings.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE RoomBookings_bookingID_seq RESTART 501; 
 
 COPY RoomRepairs
-FROM 'roomRepairs.csv'
+FROM '/extra/abogh005/project/data/roomRepairs.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE roomRepairs_repairID_seq RESTART 11;
 
 COPY RoomRepairRequests
-FROM 'roomRepairRequests.csv'
+FROM '/extra/abogh005/project/data/roomRepairRequests.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE roomRepairRequests_requestNumber_seq RESTART 11;
 
 COPY RoomUpdatesLog
-FROM 'roomUpdatesLog.csv'
+FROM '/extra/abogh005/project/data/roomUpdatesLog.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE roomUpdatesLog_updateNumber_seq RESTART 51;
